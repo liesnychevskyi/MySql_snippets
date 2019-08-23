14dc0b672613f8b731958746a269a90330ffe27b
Stanislav <liesnychevskyi.s@gmail.com>
2019-08-23 21:30:30 +0300

    1 sql

---
+++ src/main/java/sql_queries.java
@@ -0,0 +1,13 @@
+public class sql_queries
+{
+//    show databases; // shows databases on the host
+//    show tables;    // shows tables in database
+//    show columns from (name table); // shows info about table
+//    select (column name) from (table name);
+//    select (column name_1), (column name_2) from (table name);
+//    select * from (table name);
+//    select distinct (column name_1), (column name_2) from (table name); // avoiding duplicate records (just unique)
+//
+//
+
+}
